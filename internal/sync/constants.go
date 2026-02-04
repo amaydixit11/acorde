3bb575daf60171d693b7f50354e93f8f8b4f80d5
Amay Dixit <amaydixit11@gmail.com>
2026-02-04 20:05:48 +0530

    refactor(sync): isolate protocol constants

---
+++ internal/sync/constants.go
@@ -0,0 +1,7 @@
+package sync
+
+// ProtocolID is the libp2p protocol identifier for acorde sync
+const ProtocolID = "/acorde/sync/1.0.0"
+
+// ServiceName is the service name for mDNS discovery
+const ServiceName = "acorde"
